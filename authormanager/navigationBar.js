7642fa52f76d856245e29e815438bfce555689da
Schmidt Benedek Richard <schmidt.benedek@BOLYAI.LOCAL>
2026-04-24 11:22:09 +0200

    viewelement és navbar felcommentezve

--- authormanager/navigationBar.js
+++ authormanager/navigationBar.js
@@ -1,18 +1,18 @@
 import { createRadioButton } from "./gomszab.min.js";
 import { ViewElement } from "./viewElement.js";
 
-class NavigationBar extends ViewElement{
+class NavigationBar extends ViewElement{ //navigationBar osztály definíciója
     /**
      * @type {ViewElement[]}
      */
-    #viewElementList
+    #viewElementList //privát tulajdonság, ami tartalmazza a megjelenítendő viewelement leszármazottakat (táblázat, form, importexport)
 
-    constructor(){
-        super('navbar')
-        this.#viewElementList = []
-        this.div.addEventListener('change', (e) =>{
-            const radioButtonValue = e.target.value
-            this.activate(radioButtonValue)
+    constructor(){ //konstruktor definíció
+        super('navbar')//meghívjuk a szülőosztály konstruktorát
+        this.#viewElementList = [] //inicializáljuk a viewelementListet egy üres tömbbel
+        this.div.addEventListener('change', (e) =>{ //feliratkozunk a div change eseményére (mivel a div radiogombokat fog tartalmazni, ezért tudjuk figyelni a divnél, hogy melyik radiogomb lesz kijelölve)
+            const radioButtonValue = e.target.value  //elkérjük a target value értékét
+            this.activate(radioButtonValue) //meghívjuk az activate függvényt a kiválasztott radiogomb értékével (a viewelement azonosítói lehetnek, lásd: addViewElement)
         })
     }
 
@@ -21,22 +21,22 @@ class NavigationBar extends ViewElement{
      * @param {string} label 
      * @param {ViewElement} viewElement
      */
-    addViewElement(label, viewElement){
-        this.#viewElementList.push(viewElement)
-        const div = createRadioButton({id: viewElement.id, name: this.id, label})
-        this.div.appendChild(div)
+    addViewElement(label, viewElement){ //a navigationBar példányának definiál egy függvényt
+        this.#viewElementList.push(viewElement)// bemeneti viewelementet hozzáadjuk a viewelement listhez
+        const div = createRadioButton({id: viewElement.id, name: this.id, label}) //csinálunk 1 rádiógombot, amelynek az azonosítója a view element azonosítója, a labelje a függvény bemeneti paramétere, és a name, az a navigationBar azonosítója, Azért mert a radiogomboknál ha azonos a name tulajdonság csak egy választás lehetséges
+        this.div.appendChild(div) //hozzáfüzzük a divhez a radiogomb kreálás visszatérési értékét (this.div lásd Viewelement osztály definiciója)
     }
 
     /**
      * @override
      * @param {string} value 
      */
-    activate(value){
-        for(const viewElement of this.#viewElementList){
-            viewElement.activate(value)
+    activate(value){ //a szülő osztály definiál 1 activate függvényt (lásd Viewelement.activate), de a navigációs bár más logikát kell tartalmazzon
+        for(const viewElement of this.#viewElementList){ //végigiterálunk a viewelementListen(table, form, és az importexportot tartalmazza)
+            viewElement.activate(value) //meghívjuk az activate függvényét minden viewelementnek (lást ViewElement.activate)
         }
-        this.div.querySelector(`#${value}`).checked = true
+        this.div.querySelector(`#${value}`).checked = true //a diven belül lekérjük a bemeneti paraméterrel megeggyező id-jú element, és kijelöltre állítjuk
     }
 }
 
-export {NavigationBar}
+export {NavigationBar} //exportáljuk
--- authormanager/viewElement.js
+++ authormanager/viewElement.js
@@ -6,45 +6,45 @@
 import { hide, show } from "./gomszab.min.js"
 
 
-class ViewElement{
+class ViewElement{ //Őszosztály a megjelenítendő view osztályoknak
     /**
      * @type {HTMLDivElement}
      */
-    #div
+    #div //példányosításkor létrehozunk 1 divet az elemnek, azt tároljuk el benne
 
     /**
      * @type {string}
      */
-    #id
+    #id //privát tulajdonság az osztály példányának
 
     /**
      * @type {ActivateCallback}
      */
-    #activateCallback
+    #activateCallback //akkor fut le, amikor aktivvá válik (megjelenik) az adott elem (opcionális, lásd: activate függvény)
 
-    get div(){
-        return this.#div
+    get div(){ //getter definiálása a divnek
+        return this.#div //visszatér a privát div tulajdonsággal
     }
     
-    get id(){
+    get id(){ //getter az azonositonak (navigációkor használatos)
         return this.#id
     }
 
     /**
      * @param {ActivateCallback} value 
      */
-    set activateCallback(value){
-        this.#activateCallback = value
+    set activateCallback(value){ //setter az activateCallbacknek
+        this.#activateCallback = value //beállítja az activateCallbacknek a bemeneti paramétert
     }
 
     /**
      * 
      * @param {string} id 
      */
-    constructor(id){
-        this.#id = id
-        this.#div = document.createElement("div")
-        this.div.id = id
+    constructor(id){ //konstruktor, bemeneti azonosítóval
+        this.#id = id //azonosító beállítása
+        this.#div = document.createElement("div") //div létrehozása és a div privát tulajdonság beállítása
+        this.div.id = id //div azonosítójának beállítása
     }
 
     /**
@@ -52,8 +52,8 @@ class ViewElement{
      * @param {HTMLElement} parent 
      * @returns {void}
      */
-    appendTo(parent){
-        parent.appendChild(this.#div)
+    appendTo(parent){ //definiálunk 1 függvényt a példánynak, a bemeneti paraméter 1 html elem
+        parent.appendChild(this.#div)// a html elemhez hozzácsatuljuk a div tulajdonságot (lásd: konstruktor)
     }
 
     /**
@@ -61,17 +61,17 @@ class ViewElement{
      * @param {string} id 
      * @returns {void}
      */
-    activate(id){
-        if(this.#id === id){
-            show(this.#div)
-            if(this.#activateCallback){
-                this.#activateCallback()
+    activate(id){ //függvényt definiálunk a példányoknak 
+        if(this.#id === id){ //össszehasonlítjuk a bemeneti id paramétert az id tulajdonsággal
+            show(this.#div) //a divtől elveszi a hidden css osztályt
+            if(this.#activateCallback){ //ha van activateCallback
+                this.#activateCallback() //akkor meghívjuk az activateCallbacket
             }
         }
-        else{
-            hide(this.#div)
+        else{ //egyébként
+            hide(this.#div) //hozzáfűzzük az elemhez a hidden css osztályt
         }
     }
 }
 
-export {ViewElement}
+export {ViewElement} //exportáljuk a viewelementet
